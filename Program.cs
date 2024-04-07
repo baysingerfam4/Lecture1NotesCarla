e4dbd7b94f4d4bf4f8f6a02de72484cfda52a1fd
baysingerfam4 <165937206+baysingerfam4@users.noreply.github.com>
2024-04-06 20:40:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lecture1NotesCarla
+{
+    internal class Program
+    {
+        // Carla Baysinger
+        // Programming 120
+        // 04/06/2024
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Carla Baysinger");
+            Console.WriteLine("Programming 120");
+            Console.WriteLine("I love to read mystery and historical fiction");
+            // What kind of error does a red squiggly line in your code represent?
+            // Compile Time Error
+            // What is the difference between Console.Write() and Console.WriteLine()
+            // Write just creates a space where WriteLine allows it to display in the app
+            // What symbol is used to tell our code something is a string?
+            // Double Quotes
+            // What service do we use to save our code online?
+            // Git Hub
+            // When you see { and }, this indicates a bunch of code is grouped together. This is called a Curly Brace
+        }
+    }
+}
